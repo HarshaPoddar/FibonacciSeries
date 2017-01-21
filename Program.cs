a4aa13bb21ab5c7b4a0abd7489ff96672775041a
Harshvardhan Poddar <harsha.poddar@yahoo.com>
2017-01-21 14:30:11 +0530

    FibonacciSeries-Assignment2

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciSeries
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the term you need to find in the Fibonacci Series: ");
+            int Term = Convert.ToInt32(Console.ReadLine());
+            int Answer = Fibonacci(Term);
+            Console.WriteLine("The {0} Term of the Fibonacci is: " + Answer , Term);
+            Console.ReadLine();
+        }
+         static int Fibonacci(int number)
+        {
+            int sum = 0;
+            if (number <= 1)
+                return 1;
+            else
+            {
+                sum = (Fibonacci(number - 2) + Fibonacci(number - 1));
+               return sum;
+                    }
+             
+        }
+    }
+}
